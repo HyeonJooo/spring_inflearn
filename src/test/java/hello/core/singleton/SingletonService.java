0cf3b93c753eec6b9b370a2c91ea5b90ffa2e168
Hyeonju <parkhj2416@gmail.com>
2021-02-01 17:02:25 +0900

    싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonService.java
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -2,12 +2,14 @@ package hello.core.singleton;
 
 public class SingletonService {
 
+    //자기자신을 private으로 내부에서 static으로 1개 가지고 있는다.
     private static final SingletonService instance = new SingletonService();
 
     public static SingletonService getInstance() {
         return instance;
     }
 
+    //private 생성자를 써서 외부에서 생성되지 못하도록 한다.
     private SingletonService() {
     }
 
